02eb02a084c098297350016d4f8471def7514298
Vicey Wang <vicey@live.com>
2017-03-23 01:05:00 +0800

    Trivials

--- app/src/main/java/cn/vicey/navigator/Activities/MainActivity.java
+++ app/src/main/java/cn/vicey/navigator/Activities/MainActivity.java
@@ -1,4 +1,4 @@
-package cn.vicey.navigator;
+package cn.vicey.navigator.Activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import cn.vicey.navigator.R;
 
 public class MainActivity
         extends AppCompatActivity
