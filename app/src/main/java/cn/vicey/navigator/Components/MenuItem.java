33bf089c1527a9537a8f85d0e88a3d1c94fd72df
Vicey Wang <vicey@live.com>
2017-03-30 21:13:05 +0800

    Adjust style for guillotine menu

--- app/src/main/java/cn/vicey/navigator/Components/MenuItem.java
+++ app/src/main/java/cn/vicey/navigator/Components/MenuItem.java
@@ -9,10 +9,12 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import cn.vicey.navigator.R;
+import cn.vicey.navigator.Share.TypefaceManager;
 
 public class MenuItem
         extends LinearLayout
 {
+    private boolean mHighlighted;
     private ImageView mImageView;
     private TextView mTextView;
 
@@ -37,6 +39,7 @@ public class MenuItem
         LayoutInflater.from(getContext()).inflate(R.layout.menu_item, this, true);
         mImageView = (ImageView) findViewById(R.id.mi_icon);
         mTextView = (TextView) findViewById(R.id.mi_text);
+        mTextView.setTypeface(TypefaceManager.getCanaroExtraBold());
 
         if (attrs == null) return;
 
@@ -45,7 +48,14 @@ public class MenuItem
         if (icon != null) setIcon(icon);
         CharSequence text = typedArray.getText(R.styleable.Navigator_MenuItem_text);
         if (text != null) setText(text);
+        boolean highlighted = typedArray.getBoolean(R.styleable.Navigator_MenuItem_highlighted, false);
+        setHighlighted(highlighted);
         typedArray.recycle();
+    }
+
+    public boolean getHighlighted()
+    {
+        return mHighlighted;
     }
 
     public Drawable getIcon()
@@ -56,6 +66,19 @@ public class MenuItem
     public CharSequence getText()
     {
         return mTextView.getText();
+    }
+
+    public void setHighlighted(boolean value)
+    {
+        mHighlighted = value;
+        if (mHighlighted)
+        {
+            mTextView.setTextColor(getResources().getColor(R.color.highlighted));
+        }
+        else
+        {
+            mTextView.setTextColor(getResources().getColor(R.color.text));
+        }
     }
 
     public void setIcon(Drawable value)
