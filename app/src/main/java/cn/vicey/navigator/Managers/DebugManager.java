2f18243cc5ffeb7356ceb1e48320d65f53f88fc8
Vicey Wang <vicey@live.com>
2017-04-27 23:58:19 +0800

    Add and move debug settings to debug manager

--- app/src/main/java/cn/vicey/navigator/Managers/DebugManager.java
+++ app/src/main/java/cn/vicey/navigator/Managers/DebugManager.java
@@ -1,17 +1,36 @@
 package cn.vicey.navigator.Managers;
 
+import cn.vicey.navigator.Utils.Logger;
+
 /**
  * Debug manager, provides a set of methods to help debug
  */
 public final class DebugManager
 {
+    //region Constants
+
+    private static final String LOGGER_TAG = "DebugManager";
+
+    //endregion
+
     //region Static fields
 
     private static boolean mUseFakeLocationEnabled; // Whether the user node should use fake location
+    private static boolean mTrackPathEnabled;       // Whether the application should record user's path
 
     //endregion
 
     //region Static accessors
+
+    /**
+     * Gets whether the application should record user's path
+     *
+     * @return Whether the application should record user's path
+     */
+    public static boolean isTrackPathEnabled()
+    {
+        return mTrackPathEnabled;
+    }
 
     /**
      * Gets whether the user node should use fake location
@@ -24,13 +43,29 @@ public final class DebugManager
     }
 
     /**
+     * Sets whether the application should record user's path
+     *
+     * @param value Whether the application should record user's path
+     */
+    public static void setTrackPathEnabled(boolean value)
+    {
+        if (!SettingsManager.isDebugModeEnabled()) return;
+        mTrackPathEnabled = value;
+        if (value) Logger.debug(LOGGER_TAG, "Track path enabled.");
+        else Logger.debug(LOGGER_TAG, "Track path disabled.");
+    }
+
+    /**
      * Sets whether the user node should use fake location
      *
      * @param value Whether the user node should use fake location
      */
     public static void setUseFakeLocation(boolean value)
     {
+        if (!SettingsManager.isDebugModeEnabled()) return;
         mUseFakeLocationEnabled = value;
+        if (value) Logger.debug(LOGGER_TAG, "Use fake location enabled.");
+        else Logger.debug(LOGGER_TAG, "Use fake location disabled.");
     }
 
     //endregion
